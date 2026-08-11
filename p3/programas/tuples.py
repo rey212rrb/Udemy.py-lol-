279341a49670fb5d80e83eeb423c6fefff9b29b5
rey212rrb <rey212rrb@gmail.com>
2026-08-10 23:26:31 -0600

    Actividades p3 2

---
+++ p3/programas/tuples.py
@@ -0,0 +1,10 @@
+"""mi_tuple = (1,2,3,4,5,6,7,("a","b","c"),9)
+mi_tuple = list(mi_tuple)
+print(mi_tuple)"""
+
+t = (1,2,3,1,2,3,7,8,2,3,4,1,2,3)
+#x, y, z = t
+#print(y)
+#print(t.count(1))
+
+print(t.index(2))
